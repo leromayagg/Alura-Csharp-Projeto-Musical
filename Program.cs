dcc737a18fe6829f826e438a86395083e347987b
Matheus Maia <leromayagg@gmail.com>
2023-11-27 18:41:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas(){
+
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    
+    Console.WriteLine(mensagemDeBoasVindas);
+};
+
+void ExibirOpcoesDoMenu(){
+
+    Console.WriteLine("\n1 - Registrar banda");
+    Console.WriteLine("2 - Ver todas bandas");
+    Console.WriteLine("3 - Avaliar banda");
+    Console.WriteLine("4 - Média da banda");
+    Console.WriteLine("0 - Sair\n");
+
+    Console.Write("\n Digite a opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if(opcaoEscolhidaNumerica == 1){
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhidaNumerica);
+    }
+};
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
